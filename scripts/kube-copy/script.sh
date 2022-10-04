f445398014fbf8a82f223ac499d866e6afeda296
Akshay Jain <akshayjain.ecb@gmail.com>
2022-10-04 17:05:37 +0200

    add confirmation if the resource is alredy present in target

--- scripts/kube-copy/script.sh
+++ scripts/kube-copy/script.sh
@@ -12,15 +12,29 @@ while true; do
     case $yn in
         [Yy]* ) 
             kubectl config use-context "$source_cluster" >/dev/null
-            kubectl get "$object_type" "$object" -n  "$namespace" -o yaml > /tmp/"$object_type"_"$object"_"$namespace".yaml 
+            kubectl get "$object_type" "$object" -n  "$namespace" -o yaml > /tmp/kube_copy_"$object_type"_"$object"_"$namespace".yaml 
             kubectl config use-context "$destination_cluster" >/dev/null
-            validate=$(kubectl apply -f /tmp/"$object_type"_"$object"_"$namespace".yaml  -n "$namespace" --dry-run=server 2>&1 >/dev/null)
+            validate=$(kubectl apply -f /tmp/kube_copy_"$object_type"_"$object"_"$namespace".yaml  -n "$namespace" --dry-run=server 2>&1 >/dev/null)
             if echo "$validate" | ggrep -q "NotFound"; then
                 echo "namespace doesn't exist in destination. creating it now."
                 kubectl create namespace "$namespace"
             fi
-            echo "copying .."
-            kubectl apply -f /tmp/"$object_type"_"$object"_"$namespace".yaml   -n "$namespace" --dry-run="$dry_run"
+            echo "creating $object_type $object in $namespace"
+
+            if ! kubectl get "$object_type" "$object" -n "$namespace" --no-headers=true 2>/dev/null ; then
+            kubectl apply -f /tmp/kube_copy_"$object_type"_"$object"_"$namespace".yaml   -n "$namespace" --dry-run="$dry_run"
+            else
+            read -r -p "$object_type $object in $namespace already exists in target cluster, do you want to recreate ? " yn
+            case $yn in
+            [Yy]* ) 
+                kubectl config use-context "$source_cluster" >/dev/null
+                kubectl apply -f /tmp/kube_copy_"$object_type"_"$object"_"$namespace".yaml   -n "$namespace" --dry-run="$dry_run"
+                break;;
+            [Nn]* ) break;;
+                * ) echo "Please answer yes or no.";;
+            esac
+            fi
+            kubectl apply -f /tmp/kube_copy_"$object_type"_"$object"_"$namespace".yaml   -n "$namespace" --dry-run="$dry_run"
             break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
